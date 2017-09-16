b33cb8013e8357fe3284d83880293e3a35b6eb69
PKxk8J <pkxk8j@users.noreply.github.com>
2017-09-16 21:57:56 +0900

    整理

--- common.js
+++ common.js
@@ -12,30 +12,13 @@ var _export
 
   const KEY_DEBUG = 'debug'
   const KEY_NAME = 'name'
+
   const KEY_URL = 'url'
   const KEY_TITLE = 'title'
 
   const DEBUG = (i18n.getMessage(KEY_DEBUG) === 'debug')
 
   const storageArea = storage.sync
-
-  const params = {
-    storageArea,
-    KEY_URL,
-    KEY_TITLE,
-    KEY_UNIQ: 'uniq',
-    KEY_UNIQ_BY: 'uniqBy',
-    KEY_MENU_ITEM: 'menuItem',
-    KEY_NOTIFICATION: 'notification',
-    KEY_SAVE: 'save',
-    KEY_CLOSING: 'closing',
-    KEY_SUCCESS_MESSAGE: 'successMessage',
-    KEY_FAILURE_MESSAGE: 'failureMessage',
-    DEFAULT_MENU_ITEM: [KEY_URL, KEY_TITLE],
-    DEFAULT_NOTIFICATION: false,
-    NOTIFICATION_ID: i18n.getMessage(KEY_NAME),
-    DEBUG
-  }
 
   function debug (message) {
     if (DEBUG) {
@@ -55,11 +38,26 @@ var _export
     return value
   }
 
-  _export = Object.assign({
+  _export = Object.freeze({
+    KEY_URL,
+    KEY_TITLE,
+    KEY_UNIQ: 'uniq',
+    KEY_UNIQ_BY: 'uniqBy',
+    KEY_MENU_ITEM: 'menuItem',
+    KEY_NOTIFICATION: 'notification',
+    KEY_SAVE: 'save',
+    KEY_CLOSING: 'closing',
+    KEY_SUCCESS_MESSAGE: 'successMessage',
+    KEY_FAILURE_MESSAGE: 'failureMessage',
+    DEFAULT_MENU_ITEM: [KEY_URL, KEY_TITLE],
+    DEFAULT_NOTIFICATION: false,
+    NOTIFICATION_ID: i18n.getMessage(KEY_NAME),
+    DEBUG,
+    storageArea,
     debug,
     onError,
     getValue
-  }, params)
+  })
 }
 
 const common = _export
